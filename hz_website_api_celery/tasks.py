47fc61e44e8e4bef5f0bc265dd22897bfd88a0ec
zhangcong <18511123018@163.com>
2019-03-28 11:32:47 +0800

    调试定时发布任务功能

--- hz_website_api_celery/tasks.py
+++ hz_website_api_celery/tasks.py
@@ -8,10 +8,16 @@ from .celery import app
 import requests
 import datetime
 
-# HOST = 'http://127.0.0.1:8001'
-# HOST = 'http://xmgl.zhugeyingxiao.com'
 from hurong import models
 from publicFunc.send_email import SendEmail
+
+
+@app.task
+def test():
+    task_list_objs = models.TaskList.objects.exclude(percentage_progress=100).filter(is_delete=False)[0: 1]
+    if task_list_objs:
+        task_list_objs.update(status=1)
+        print("task_list_objs -->", task_list_objs)
 
 
 # 发送邮件，每间隔5分钟一次
@@ -19,9 +25,11 @@ from publicFunc.send_email import SendEmail
 def hurong_send_email():
     task_list_objs = models.TaskList.objects.exclude(percentage_progress=100).filter(is_delete=False)[0: 1]
     if task_list_objs:
-        task_list_objs.update(status=3)
         # 发送标题和内容
         task_list_obj = task_list_objs[0]
+        task_list_obj.status = 2
+        task_list_obj.save()
+
         send_email_title = task_list_obj.send_email_title
         send_email_content = task_list_obj.send_email_content
 
@@ -59,31 +67,7 @@ def hurong_send_email():
                 email_user_obj.is_available = False
                 email_user_obj.save()
 
-        # while True:
-        #     # 获取发送账号
-        #     email_user_obj = models.EmailUserInfo.objects.all().order_by('use_number')[0]
-        #     email_user_obj.use_number += 1
-        #     email_user_obj.save()
-        #     email_user = email_user_obj.email_user
-        #     email_pwd = email_user_obj.email_pwd
-        #
-        #     # 开始发送
-        #     send_email_obj = SendEmail(
-        #         email_user,
-        #         email_pwd,
-        #         send_email_list,
-        #         send_email_title,
-        #         send_email_content
-        #     )
-        #     send_email_obj.send_email()
-        #     print("发送成功")
-        #
-        #     # 计算完成百分比
-        #     models.TaskInfo.objects.filter(id__in=task_info_id_list).update(status=2)
-        #     task_objs = models.TaskInfo.objects.filter(task_list=task_list_obj)
-        #     task_list_obj.percentage_progress = int(task_objs.filter(status=2).count() / task_objs.count())
-        #     task_list_obj.save()
 
-
-
+if __name__ == '__main__':
+    test()
 
