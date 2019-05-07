d2837fe69219a29637f29aaf5371b87578eece90
张聪 <18511123018@163.com>
2019-05-07 17:03:01 +0800

    增加获取任务是否已经找到覆盖信息的接口

--- hurong/views_dir/xiaohongshufugai.py
+++ hurong/views_dir/xiaohongshufugai.py
@@ -151,6 +151,43 @@ def xiaohongshufugai_oper(request, oper_type, o_id):
                 #     response.code = 300
                 #     response.msg = "该任务在操作中或者已完成，不能删除"
 
+        # 手机端当前任务是否已经查询到排名
+        elif oper_type == "is_selected_rank":
+            form_data = {
+                'keywords': request.POST.get('keywords'),
+                'url': request.POST.get('url')
+            }
+            #  创建 form验证 实例（参数默认转成字典）
+            forms_obj = IsSelectedRankForm(form_data)
+            if forms_obj.is_valid():
+                print("验证通过")
+
+                keywords = forms_obj.cleaned_data.get('keywords')
+                url = forms_obj.cleaned_data.get('url')
+
+                # 5分钟之前的时间
+                five_minutes_ago_data = datetime.datetime.now() - datetime.timedelta(minutes=5)
+                objs = models.XiaohongshuFugaiDetail.objects.select_related('keywords').filter(
+                    keywords__keywords=keywords,
+                    keywords__url=url,
+                    update_datetime__gt=five_minutes_ago_data
+                )
+
+                is_selected = False
+                if objs:  # 说明已经找到排名了
+                    is_selected = True
+
+                response.code = 200
+                response.data = {
+                    "is_selected": is_selected
+                }
+                response.msg = "查询成功"
+
+            else:
+                print("验证不通过")
+                response.code = 301
+                response.msg = json.loads(forms_obj.errors.as_json())
+
     else:
         # 下拉词详情
         if oper_type == "detail":
@@ -223,42 +260,6 @@ def xiaohongshufugai_oper(request, oper_type, o_id):
             if item:
                 response.data = json.loads(item.decode('utf8'))
 
-        # 手机端当前任务是否已经查询到排名
-        elif oper_type == "is_selected_rank":
-            form_data = {
-                'keywords': request.GET.get('keywords'),
-                'url': request.POST.get('url')
-            }
-            #  创建 form验证 实例（参数默认转成字典）
-            forms_obj = IsSelectedRankForm(form_data)
-            if forms_obj.is_valid():
-                print("验证通过")
-
-                keywords = forms_obj.cleaned_data.get('keywords')
-                url = forms_obj.cleaned_data.get('url')
-
-                # 5分钟之前的时间
-                five_minutes_ago_data = datetime.datetime.now() - datetime.timedelta(minutes=5)
-                objs = models.XiaohongshuFugaiDetail.objects.select_related('keywords').filter(
-                    keywords__keywords=keywords,
-                    keywords__url=url,
-                    update_datetime__gt=five_minutes_ago_data
-                )
-
-                is_selected = False
-                if objs:    # 说明已经找到排名了
-                    is_selected = True
-
-                response.code = 200
-                response.data = {
-                    "is_selected": is_selected
-                }
-                response.msg = "查询成功"
-
-            else:
-                print("验证不通过")
-                response.code = 301
-                response.msg = json.loads(forms_obj.errors.as_json())
         else:
             response.code = 402
             response.msg = "请求异常"
