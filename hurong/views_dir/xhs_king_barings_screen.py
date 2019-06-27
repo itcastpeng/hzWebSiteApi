092bbcf743b66da75fbf1e6ae701f2e92cd7aa91
zhaoxinpeng <15531506906@sina.cn>
2019-06-27 16:41:49 +0800

    重查覆盖 功能

--- hurong/views_dir/xhs_king_barings_screen.py
+++ hurong/views_dir/xhs_king_barings_screen.py
@@ -14,27 +14,26 @@ def xhs_king_barings_screen(request, oper_type):
 
         # 小红书 霸屏王 重查覆盖
         if oper_type == 'get_xhs_account':
-            id = request.POST.get('id')
+            user_id_list = request.POST.get('user_id_list')
             now = datetime.datetime.today()
             code = 200
-            if not id:
-                models.xhs_bpw_keywords.objects.all().update(
-                    update_datetime=None
-                )
-                models.xhs_bpw_fugai.objects.filter(create_datetime__gte=now).delete()
-                msg = '全部关键词重查成功, 请耐心等待'
-            else:
-                keywords_objs = models.xhs_bpw_keywords.objects.filter(id=id)
-                if keywords_objs:
+
+            user_id_list = json.loads(user_id_list)
+            if len(user_id_list) >= 1:
+                for user_id in user_id_list:
+                    keywords_objs = models.xhs_bpw_keywords.objects.filter(uid=user_id)
                     keywords_objs.update(
                         update_datetime=None
                     )
-                    keywords_obj = keywords_objs[0]
-                    models.xhs_bpw_fugai.objects.filter(keywords_id=keywords_obj.id).delete()
-                    msg = '关键词《{}》重查成功, 请耐心等待'.format(keywords_obj.keywords)
-                else:
-                    code = 301
-                    msg = '关键词不存在'
+                    for keywords_obj in keywords_objs:
+                        models.xhs_bpw_fugai.objects.filter(
+                            create_datetime__gte=now,
+                            keywords_id=keywords_obj.id
+                        ).delete()
+                msg = '重查成功, 请耐心等待'
+            else:
+                code = 301
+                msg = '重查失败,原因:用户ID不存在'
 
             response.code = code
             response.msg = msg
