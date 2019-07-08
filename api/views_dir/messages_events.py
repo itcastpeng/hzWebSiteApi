6fa1d61642be7c368c39ef1432c0a4e8d340549a
zhaoxinpeng <15531506906@sina.cn>
2019-07-08 11:25:48 +0800

    优化 告警提醒  评论管理优化

--- api/views_dir/messages_events.py
+++ api/views_dir/messages_events.py
@@ -12,7 +12,7 @@ from django.http import JsonResponse
 def messages_events_oper(request, oper_type, appid):
     response = Response.ResponseObj()
 
-    #
+    # 消息与事件接收
     if oper_type == 'callback':
         pass
 
