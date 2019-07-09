8d5f6098b9de437d6c10904f57fd3373caf68f38
zhaoxinpeng <15531506906@sina.cn>
2019-07-09 17:40:05 +0800

    三方平台 接收数据 测试

--- api/views_dir/tripartite_platform.py
+++ api/views_dir/tripartite_platform.py
@@ -16,9 +16,10 @@ def tripartite_platform_oper(request, oper_type):
 
         # 授权事件接收  （微信后台10分钟一次回调该接口 传递component_verify_ticket）
         if oper_type == 'tongzhi':
-            models.TripartitePlatform.objects.filter(
+            objs = models.TripartitePlatform.objects.filter(
                 appid__isnull=False
-            ).update(component_access_token=55555555)
+            )
+            objs.update(component_access_token=1111)
 
             signature = request.GET.get('signature')
             timestamp = request.GET.get('timestamp')
@@ -29,18 +30,22 @@ def tripartite_platform_oper(request, oper_type):
 
             xml_tree = ET.fromstring(postdata)
             Encrypt = xml_tree.find('Encrypt').text
+            objs.update(component_access_token=2222)
+            wx_obj = WXBizMsgCrypt('sisciiZiJCC6PuGOtFWwmDnIHMsZyX', 'sisciiZiJCC6PuGOtFWwmDnIHMsZyXmDnIHMsZyX123', 'wx1f63785f9acaab9c')
+            try:
+                ret, decryp_xml = wx_obj.DecryptMsg(Encrypt, msg_signature, timestamp, nonce)
+                objs.update(component_access_token=333)
+                decryp_xml_tree = ET.fromstring(decryp_xml)
+                ComponentVerifyTicket = decryp_xml_tree.find("ComponentVerifyTicket").text
+                models.TripartitePlatform.objects.filter(
+                    appid=xml_tree.find('AppId').text
+                ).update(
+                    component_verify_ticket=ComponentVerifyTicket,
+                    linshi=decryp_xml
+                )
+            except Exception as e:
+                objs.update(component_access_token=4444, linshi=e)
 
-            wx_obj = WXBizMsgCrypt('sisciiZiJCC6PuGOtFWwmDnIHMsZyX', 'dd9f731252b32353844feffed5172f2c', 'wx1f63785f9acaab9c')
-            ret, decryp_xml = wx_obj.DecryptMsg(Encrypt, msg_signature, timestamp, nonce)
-            decryp_xml_tree = ET.fromstring(decryp_xml)
-            ComponentVerifyTicket = decryp_xml_tree.find("ComponentVerifyTicket").text
-
-            models.TripartitePlatform.objects.filter(
-                appid=xml_tree.find('AppId').text
-            ).update(
-                component_verify_ticket=ComponentVerifyTicket,
-                linshi=decryp_xml
-            )
 
             return HttpResponse('success')
     else:
