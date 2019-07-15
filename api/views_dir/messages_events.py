7acd40541504ea4e1a9ba88ea63a9daab85d8019
zhaoxinpeng <15531506906@sina.cn>
2019-07-15 17:11:37 +0800

    优化评论管理 参数

--- api/views_dir/messages_events.py
+++ api/views_dir/messages_events.py
@@ -1,11 +1,11 @@
 
 
 from api import models
-from publicFunc import Response
-from publicFunc import account
+from publicFunc import Response, account
 from django.http import JsonResponse
-
-
+from publicFunc.tripartite_platform_oper import encoding_appid, encodingAESKey, encoding_token
+from wechatpy.crypto import WeChatCrypto
+import xml.etree.cElementTree as ET, xml.dom.minidom as xmldom, requests
 
 
 
@@ -14,14 +14,35 @@ def messages_events_oper(request, oper_type, appid):
 
     # 消息与事件接收
     if oper_type == 'callback':
-        pass
+        timestamp = request.GET.get('timestamp')
+        nonce = request.GET.get('nonce')
+        msg_signature = request.GET.get('msg_signature')
+        postdata = request.body.decode(encoding='UTF-8')
+        # xml_tree = ET.fromstring(postdata)
+        # encrypt = xml_tree.find("Encrypt").text
+
+        crypto = WeChatCrypto(encoding_token, encodingAESKey, encoding_appid)
+        decrypted_xml = crypto.decrypt_message(
+            postdata,
+            msg_signature,
+            timestamp,
+            nonce
+        )
+        DOMTree = xmldom.parseString(decrypted_xml)
+        collection = DOMTree.documentElement
+        MsgType = collection.getElementsByTagName("MsgType")[0].childNodes[0].data
+        if MsgType == 'event': # 消息类型
+            Event = collection.getElementsByTagName("Event")[0].childNodes[0].data
+            ToUserName = collection.getElementsByTagName("ToUserName")[0].childNodes[0].data
+            FromUserName = collection.getElementsByTagName("FromUserName")[0].childNodes[0].data
+            CreateTime = collection.getElementsByTagName("CreateTime")[0].childNodes[0].data
+            Reason = collection.getElementsByTagName("Reason")[0].childNodes[0].data
+            FailTime = collection.getElementsByTagName("FailTime")[0].childNodes[0].data
+            ScreenShot = collection.getElementsByTagName("ScreenShot")[0].childNodes[0].data
 
 
-
-
-
-
-
+        else:
+            pass
 
 
     return JsonResponse(response.__dict__)
