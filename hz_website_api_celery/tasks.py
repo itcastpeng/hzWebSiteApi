a420bdd49760ecb90fa3af726eb78e6cc8799109
congzhang <652314341@qq.com>
2019-07-29 20:17:06 +0800

    调试获取查覆盖任务

--- hz_website_api_celery/tasks.py
+++ hz_website_api_celery/tasks.py
@@ -123,7 +123,6 @@ def xiaohongshu_fugai_update_data():
 
             # 更新霸屏王查覆盖数据
             objs = models.xhs_bpw_keywords.objects.filter(keywords=keywords)
-            objs.update(update_datetime=datetime.datetime.now())
             for obj in objs:
                 # obj.update_datetime = datetime.datetime.now()
                 # obj.save()
@@ -143,6 +142,7 @@ def xiaohongshu_fugai_update_data():
                             rank=rank,
                             biji_num=total_count
                         )
+            objs.update(update_datetime=datetime.datetime.now())
 
             # 更新下拉笔记数据
             models.XiaohongshuXiaLaKeywords.objects.filter(keywords=keywords).update(
