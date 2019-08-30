fcaf5bad252bdddd66fcc1587f56eac5b34d2d40
zhaoxinpeng <15531506906@sina.cn>
2019-08-30 11:55:31 +0800

    快速建站优化

--- api/celery_url.py
+++ api/celery_url.py
@@ -5,5 +5,4 @@ from api.views_dir import celery_management
 urlpatterns = [
     # 定时刷新转接 时间是否过期
     url(r'^time_refresh_switch$', celery_management.time_refresh_whether_connect_time_expired),
-
 ]
