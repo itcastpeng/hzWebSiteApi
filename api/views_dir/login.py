a6c825e3c8080d89c8e5e9acf8d70bb513d49c44
zhaoxinpeng <15531506906@sina.cn>
2019-10-22 13:57:41 +0800

    叮咚平台 跳转创建用户

--- api/views_dir/login.py
+++ api/views_dir/login.py
@@ -138,6 +138,7 @@ def external_login(request):
             obj = objs[0]
 
         else:
+            print('user_data---------> ', user_data)
             obj = models.UserProfile.objects.create(**user_data)
             get_user_info_url = 'http://a.yingxiaobao.org.cn/api/user/info/{}?token={}&user_id={}'.format(
                 userId, external_token, userId
