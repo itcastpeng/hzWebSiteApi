efdd12aa609ab736501bf301786e1491757c6ccc
zhaoxinpeng <15531506906@sina.cn>
2019-11-06 18:06:32 +0800

    提交代码包

--- api/views_dir/baidu_tripartite_platform_management.py
+++ api/views_dir/baidu_tripartite_platform_management.py
@@ -78,16 +78,15 @@ def tripartite_platform_oper(request, oper_type):
             current_page = request.GET.get('current_page', 1)
             length = request.GET.get('length', 10)
             response = tripartite_platform_oper.get_template_list(current_page, length)
-            print("-response.data.get('list')---------> ", response.data.get('list'))
+            response_data = response.data.get('list')[0]
+            template_id = response_data.get('template_id')
 
-            # response_data = tripartite_platform_oper.gets_list_small_packages(token)
-            # package_id = response_data.data[0].get('package_id')
-            # data = {
-            #     'content': request.POST.get('content'),
-            #     'package_id': package_id,
-            #     'remark': request.POST.get('remark'),
-            # }
-            # response = tripartite_platform_oper.submit_approval_authorized_mini_program(data)
+            data = {
+                'content': request.POST.get('content'),
+                'package_id': template_id,
+                'remark': request.POST.get('remark'),
+            }
+            response = tripartite_platform_oper.submit_approval_authorized_mini_program(data)
 
         # 发布已审核的小程序
         elif oper_type == 'release_approved_applet':
