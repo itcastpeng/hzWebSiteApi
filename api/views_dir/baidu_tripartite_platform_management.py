432c24b494e90491fb2c04446440a89ca024baae
zhaoxinpeng <15531506906@sina.cn>
2019-11-06 18:05:09 +0800

    提交代码包

--- api/views_dir/baidu_tripartite_platform_management.py
+++ api/views_dir/baidu_tripartite_platform_management.py
@@ -75,14 +75,19 @@ def tripartite_platform_oper(request, oper_type):
 
         # 为授权的小程序提交审核
         elif oper_type == 'submit_approval_authorized_mini_program':
-            response_data = tripartite_platform_oper.gets_list_small_packages(token)
-            package_id = response_data.data[0].get('package_id')
-            data = {
-                'content': request.POST.get('content'),
-                'package_id': package_id,
-                'remark': request.POST.get('remark'),
-            }
-            response = tripartite_platform_oper.submit_approval_authorized_mini_program(data)
+            current_page = request.GET.get('current_page', 1)
+            length = request.GET.get('length', 10)
+            response = tripartite_platform_oper.get_template_list(current_page, length)
+            print("-response.data.get('list')---------> ", response.data.get('list'))
+
+            # response_data = tripartite_platform_oper.gets_list_small_packages(token)
+            # package_id = response_data.data[0].get('package_id')
+            # data = {
+            #     'content': request.POST.get('content'),
+            #     'package_id': package_id,
+            #     'remark': request.POST.get('remark'),
+            # }
+            # response = tripartite_platform_oper.submit_approval_authorized_mini_program(data)
 
         # 发布已审核的小程序
         elif oper_type == 'release_approved_applet':
