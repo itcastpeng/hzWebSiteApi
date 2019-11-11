d8d9e39b8d61af8bbcacb303d1a86de0316f96dd
张聪 <18511123018@163.com>
2019-11-11 09:55:24 +0800

    调试路由配置文件

--- hzWebSiteApi/urls.py
+++ hzWebSiteApi/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    url('api/', include('api.urls')),
-    url('api_hurong/', include('hurong.urls', 'hurong'), "hurong"),
+    url(r'^api/', include('api.urls')),
+    url(r'^api_hurong/', include('hurong.urls')),
 ]
