2b66403cbea0260fdd512ec2f19191984230fd78
张聪 <18511123018@163.com>
2019-11-11 10:09:30 +0800

    调试路由配置文件

--- hzWebSiteApi/settings.py
+++ hzWebSiteApi/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+from hzWebSiteApi import params
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-print("BASE_DIR -->", BASE_DIR)
+print("params.DEBUG -->", params.DEBUG)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
