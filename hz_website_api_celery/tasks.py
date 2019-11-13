0bba13353c42fe7c374402ae6a9fe824ff389414
张聪 <18511123018@163.com>
2019-11-13 16:13:22 +0800

    优化霸屏王笔记链接问题

--- hz_website_api_celery/tasks.py
+++ hz_website_api_celery/tasks.py
@@ -734,7 +734,9 @@ def zhangcong_test():
     objs = models.xhs_bpw_biji_url.objects.filter(biji_url__contains="xhsurl.com")
     for obj in objs:
         biji_url = obj.biji_url.strip().replace('，复制本条信息，打开【小红书】App查看精彩内容！', '').rstrip("，")
-        print(biji_url)
+        ret = requests.get(biji_url)
+        print(ret.url)
+        break
 
 
 
