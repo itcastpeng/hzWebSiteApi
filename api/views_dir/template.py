2fa757e910a0fec95b65953e9e9a3dfd7c5c6c38
张聪 <18511123018@163.com>
2019-11-19 10:36:40 +0800

    调试保存历史版本

--- api/views_dir/template.py
+++ api/views_dir/template.py
@@ -469,18 +469,12 @@ def template_oper(request, oper_type, o_id):
                 template_data["pages_data"].append(page_data)
 
             redis_data = redis_obj.get(redis_key)
-            print("redis_data -->", redis_data)
             if redis_data:
                 redis_data = json.loads(redis_data)
             else:
                 redis_data = []
-            # print("redis_data -->", redis_data)
-            # print("template_data -->", template_data)
-            print("redis_data -->", redis_data)
-            redis_data = redis_data.append(template_data)
-            print("redis_data -->", redis_data)
+            redis_data.append(template_data)
             redis_data = json.dumps(redis_data)
-            print("redis_data -->", redis_data)
             redis_obj.set(redis_key, redis_data)
 
             response.code = 200
