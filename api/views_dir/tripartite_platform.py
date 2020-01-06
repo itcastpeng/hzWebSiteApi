7dca288e11b38c9b5f25b1df1f980a6ae6d19acc
张聪 <18511123018@163.com>
2020-01-06 14:22:09 +0800

    调试三方平台

--- api/views_dir/tripartite_platform.py
+++ api/views_dir/tripartite_platform.py
@@ -1112,8 +1112,8 @@ def tongzhi(request):
     )
     objs.update(linshi=postdata)
     wx_obj = WXBizMsgCrypt(encoding_token, encodingAESKey, appid)
-    ret, decryp_xml = wx_obj.DecryptMsg(postdata, msg_signature, timestamp, nonce)
-    # ret, decryp_xml = wx_obj.DecryptMsg(Encrypt, msg_signature, timestamp, nonce)
+    # ret, decryp_xml = wx_obj.DecryptMsg(postdata, msg_signature, timestamp, nonce)
+    ret, decryp_xml = wx_obj.DecryptMsg(Encrypt, msg_signature, timestamp, nonce)
     decryp_xml_tree = ET.fromstring(decryp_xml)
     oper_type = decryp_xml_tree.find("InfoType").text
     print('*******************//////////*******************/////////******************')
